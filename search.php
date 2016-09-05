34b91848130266854317e8ac1daafd19d9cc9410
InterP <korex527@naver.com>
2016-09-05 20:24:20 +0900

    First commit, change README, add logo, add awkward search

--- search.php
+++ search.php
@@ -55,64 +55,59 @@
 
 <?php
 	/*-----------------------------------------------------------------------------------*/
-	/* Start Home loop
+	/* Start Search loop
 	/*-----------------------------------------------------------------------------------*/
-	
-	if( is_home() || is_archive() ) {
-	
 ?>
-			<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
+			<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'casper' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-					<article class="post">
+				<article class="post">
+				
+					<h1 class="title">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+							<?php the_title() ?>
+						</a>
+					</h1>
+					<div class="post-meta">
+						<?php if( comments_open() ) : ?>
+							<span class="comments-link">
+								<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
+							</span>
+						<?php endif; ?>
 					
-						<h1 class="title">
-							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-								<?php the_title() ?>
-							</a>
-						</h1>
-						<div class="post-meta">
-							<?php if( comments_open() ) : ?>
-								<span class="comments-link">
-									<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
-								</span>
-							<?php endif; ?>
+					</div><!--/post-meta -->
+					
+					<div class="the-content">
+						<?php the_content( 'Continue...' ); ?>
 						
-						</div><!--/post-meta -->
-						
-						<div class="the-content">
-							<?php the_content( 'Continue...' ); ?>
-							
-							<?php wp_link_pages(); ?>
-						</div><!-- the-content -->
-						
-						<div class="meta clearfix">
-							<div class="category"><?php echo get_the_category_list(); ?></div>
-							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
-						</div><!-- Meta -->
-						
-					</article>
-
-				<?php endwhile; ?>
-				
-				<!-- pagintation -->
-				<div id="pagination" class="clearfix">
-					<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
-					<div class="next-page"><?php next_posts_link( ' &laquo; Older' ); ?></div>
-				</div><!-- pagination -->
-
-
-			<?php else : ?>
-				
-				<article class="post error">
-					<h1 class="404">Nothing posted yet</h1>
+						<?php wp_link_pages(); ?>
+					</div><!-- the-content -->
+					
+					<div class="meta clearfix">
+						<div class="category"><?php echo get_the_category_list(); ?></div>
+						<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
+					</div><!-- Meta -->
+					
 				</article>
 
-			<?php endif; ?>
+			<?php endwhile; ?>
+			
+			<!-- pagintation -->
+			<div id="pagination" class="clearfix">
+				<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
+				<div class="next-page"><?php next_posts_link( ' &laquo; Older' ); ?></div>
+			</div><!-- pagination -->
 
-		
-	<?php } //end is_home(); ?>
+
+		<?php else : ?>
+			
+			<article class="post error">
+				<h1 class="404">Nothing posted yet</h1>
+			</article>
+
+		<?php endif; ?>
 
 <?php
 	/*-----------------------------------------------------------------------------------*/
